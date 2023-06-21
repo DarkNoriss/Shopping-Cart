996e05b38225948e3c0b80642725ca9b5c6f6b2a
Patryk Przybylski <darknoriss@gmail.com>
2023-06-21 20:23:00 +0200

    add display products from database

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,22 @@
+import { StoreItem } from '@/components/StoreItem';
+import storeItems from '@/data/items.json';
+
 const Products = () => {
   return (
-    <div className='flex w-2/3'>
-      <div className='w-1/4'>Filters</div>
-      <div className='flex-1'>
-        <div>Products</div>
+    <div>
+      <h3>Products</h3>
+      <div className='grid flex-1 grid-cols-3 gap-4'>
+        {storeItems.map((item) => (
+          <div
+            className='flex h-48 flex-col justify-end rounded-lg bg-gray-600 p-1'
+            key={item.id}
+          >
+            <StoreItem {...item} />
+          </div>
+        ))}
       </div>
     </div>
   );
 };
+
 export default Products;
