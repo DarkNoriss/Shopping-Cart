032cfc2e4d6d5e7297cbf56eae40d13bad5de645
Patryk Przybylski <darknoriss@gmail.com>
2023-06-21 20:22:46 +0200

    change styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 const Home = () => {
   return (
-    <div className='flex gap-12'>
-      <div className='flex h-72 w-64 items-center justify-center rounded-2xl border-2'>SOME AD</div>
-      <div className='flex-1 border-b-2 border-t-2'>
-        <h2 className=''>Recommended</h2>
+    <>
+      <div className='flex h-72 w-full gap-12'>
+        <div className='flex h-72 w-64 items-center justify-center rounded-2xl border-2'>
+          SOME AD
+        </div>
+        <div className='flex-1 border-b-2 border-t-2'>
+          <h2 className='mt-2'>Recommended</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
 export default Home;
