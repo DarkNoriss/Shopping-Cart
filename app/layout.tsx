5c7d221cd70ca51a980035732412cb1ca8118073
Patryk Przybylski <darknoriss@gmail.com>
2023-06-25 19:38:58 +0200

    add scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <ShoppingCartProvider>
           <main className='flex h-screen min-h-full w-screen flex-col overflow-hidden bg-gray-800 px-16'>
             <Header />
-            <div className='flex flex-1 justify-center'>{children}</div>
+            <div className='flex flex-1 justify-center overflow-auto'>
+              {children}
+            </div>
             <Footer />
           </main>
         </ShoppingCartProvider>
