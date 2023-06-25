091361bdafb6e2b87505b549f39d7d1566916852
Patryk Przybylski <darknoriss@gmail.com>
2023-06-25 19:40:56 +0200

    change name, width

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,7 +1,7 @@
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const ProductLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex w-2/3'>
-      <div className='mr-4 w-1/4 rounded-xl border-2'>
+      <div className='mr-4 w-52 rounded-xl border-2'>
         <h3 className='bold m-4'>Filters</h3>
       </div>
       {children}
@@ -9,4 +9,4 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default RootLayout;
+export default ProductLayout;
